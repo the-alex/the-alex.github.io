6496c9d6348a2dc83c06373b99432d67d537b4ea
Alex Chojnacki <thealex@umich.edu>
2020-10-29 17:30:55 -0400

    refactor: Giving good people bad ideas

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@ set -euo pipefail
 
 statik compile -d . src/site.clj
 
-git add -u && git commit -m "deps: statik build artifacts" && git push --force
+git add -u && git commit -m "deps: statik build artifacts" && git push
